0b60a28875cc27549918ecc7491126d398734c08
Zyp655 <leehieu655@gmail.com>
2025-09-25 20:59:11 +0700

    items detail

---
+++ lib/View/User/items_detail_screen.dart
@@ -0,0 +1,356 @@
+import 'package:e_commerce/Models/model.dart';
+import 'package:e_commerce/Utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ItemsDetailScreen extends StatefulWidget {
+  final AppModel eCommerceApp;
+  const ItemsDetailScreen({super.key, required this.eCommerceApp});
+  
+  @override
+  State<ItemsDetailScreen> createState()=>_ItemsDetailScreen();
+}
+
+class _ItemsDetailScreen extends State<ItemsDetailScreen> {
+  int currentIndex =0;
+  int selectedColorIndex=1;
+  int selectedSizeIndex=1;
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: fbackgroundColor2,
+        title: const Text('Detail Product'),
+        actions: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Icon(
+                Iconsax.shopping_bag,
+                size: 28,
+              ),
+              Positioned(
+                right: -3,top: -5 ,
+                child: Container(
+                  padding: EdgeInsets.all(4) ,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(width: 20,),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            color: fbackgroundColor2,
+            height: size.height *0.46,
+            width: size.width,
+            child: PageView.builder(
+              onPageChanged: (value){
+                setState(() {
+                  currentIndex==value;
+                });
+              },
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context,index){
+                  return Column(
+                    children: [
+                    Image.asset(
+                      width: size.width *0.85,
+                      widget.eCommerceApp.image,
+                      height: size.height *0.4,
+                      fit: BoxFit.cover,
+                    ),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                            3,
+                            (index)=> AnimatedContainer(
+                              duration: const Duration(microseconds: 300),
+                              margin: const EdgeInsets.only(right: 4),
+                              width: 7,
+                              height: 7,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: index == currentIndex
+                                  ? Colors.blue
+                                  : Colors.grey.shade300,
+                              ),
+                            ),
+                        ),
+                      )
+                    ],
+                  );
+                },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'H&M',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black26,
+                      ),
+                    ),
+
+                    const SizedBox(height: 5,),
+
+                    Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                      size: 17,
+                    ),
+                    Text(widget.eCommerceApp.rating.toString()),
+                    Text(
+                      '(${widget.eCommerceApp.review})',
+                      style: TextStyle(
+                        color: Colors.black26,
+                      ),
+                    ),const Spacer(),
+                    Icon(Icons.favorite_border),
+                  ],
+                ),
+                Text(
+                    widget.eCommerceApp.name,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      height: 1.5,
+                    ),
+                  ),
+                Row(
+                  children: [
+                    Text(
+                      '\$${widget.eCommerceApp.price.toString()}.00',
+                      style: const TextStyle(
+                        color: Colors.pinkAccent,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        height: 1.5,
+                      ),
+                    ),
+                    const  SizedBox(width: 5,),
+                    if(widget.eCommerceApp.isCheck== true)
+                      Text(
+                        '\$${widget.eCommerceApp.price + 200}.00',
+                        style: const TextStyle(
+                          color: Colors.black26,
+                          decoration: TextDecoration.lineThrough,
+                          decorationColor: Colors.black26,
+                        ),
+                      ),
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Text(
+                  '$myDescription1 ${widget.eCommerceApp.name}',
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: -.5,
+                    color: Colors.black38,
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width / 2.1 ,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Color',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection:Axis.horizontal ,
+                            child: Row(
+                              children: widget.eCommerceApp.fcolor
+                                  .asMap()
+                                  .entries
+                                  .map((entry) {
+                                    final int index=entry.key;
+                                    final color=entry.value;
+                                return Padding(
+                                  padding: EdgeInsets.only(top:10,right: 10),
+                                  child: CircleAvatar(
+                                    radius: 18,
+                                    backgroundColor: color,
+                                    child: InkWell(
+                                      onTap: (){
+                                        selectedColorIndex=index;
+                                      },
+                                      child: Icon(
+                                        Icons.check,
+                                        color: selectedColorIndex == index
+                                            ?Colors.white
+                                            :Colors.transparent,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width / 2.1 ,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Size',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection:Axis.horizontal ,
+                            child: Row(
+                              children: widget.eCommerceApp.size
+                                  .asMap()
+                                  .entries
+                                  .map((entry) {
+                                final int index=entry.key;
+                                final String size=entry.value;
+                                return GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      selectedSizeIndex = index;
+                                    });
+                                  },
+                                  child: Container(
+                                    margin:const EdgeInsets.only(top:10,right:10),
+                                    height: 35,
+                                    width: 35,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: selectedSizeIndex == index
+                                        ? Colors.black
+                                        : Colors.white,
+                                      border:Border.all(
+                                        color: selectedSizeIndex==index
+                                            ? Colors.black
+                                            : Colors.black12,
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        size,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: selectedSizeIndex==index
+                                              ? Colors.black
+                                              : Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+
+              ],
+            ),
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){},
+        backgroundColor: Colors.white,
+        elevation: 0,
+        label: SizedBox(
+          width: size.width *0.9,
+          child: Row(
+            children: [
+              Expanded(
+                  child:Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                      border:Border.all(color: Colors.black),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Iconsax.shopping_bag,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 5,),
+                        Text(
+                          'Add To Cart',
+                          style: TextStyle(
+                            color: Colors.black,
+                            letterSpacing: -1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ),
+              SizedBox(width: 10,),
+              Expanded(
+                child:Container(
+                  padding:const EdgeInsets.symmetric(vertical: 18),
+                  color: Colors.black,
+                  child: const Center(
+                    child: Text(
+                      'Buy Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: -1,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
